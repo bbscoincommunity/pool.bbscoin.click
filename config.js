4aca29b09817a29291792309089524e52120f2ab
Jou M <djem@hotmail.com>
2021-05-09 14:04:20 -0400

    Upload frontend assets

---
+++ config.js
@@ -0,0 +1,48 @@
+var api = "https://rpc.bbscoin.click";
+
+var api_blockexplorer = "https://explorer.bbscoin.click";
+
+var poolHost = "mine.bbscoin.click";
+
+var irc = "irc.freenode.net/#poolhost";
+
+var email = "support@bbscoin.click";
+
+var cryptonatorWidget = [];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "https://explorer.bbscoin.click/?hash={id}#blockchain_block";
+
+var transactionExplorer = "https://explorer.bbscoin.click/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = {
+    "bbs": [
+		['pool.bbscoin.click', 'https://rpc.bbscoin.click'],
+		['bbs.gonspool.com', 'https://bbs.gonspool.com/api'],
+		['bbscoin.my-mining-pool.de', 'https://apibbscoin.my-mining-pool.de'],
+		['webbbs.semipool.com', 'https://webbbs.semipool.com/api/all/network'],
+		['bbs.soloway.in.ua','https://www.bbscoin.click/ext.cgi?bbs.soloway.in.ua:8117']
+
+    ]
+};
+
+/*
+		['pool.bbscoin.xyz', 'https://www.bbscoin.click/ext.cgi?pool.bbscoin.xyz:8117'],
+		['cryptoknight.cc/bbs', 'http://78.46.85.142:2778'],
+		['bbs.gonspool.com', 'http://bbs.gonspool.com:8117'],
+		['pool-marine.ga/bbs', 'http://pool-marine.ga:8028'],
+		['bbs.howpool.com', 'http://bbs.howpool.com:8117'],
+		['bbscoin-pool.org', 'http://bbscoin-pool.org:8107'],
+		['149.129.129.195', 'http://149.129.129.195:8117'],
+		['bbs.newpool.pw', 'http://bbs.newpool.pw:8122'],
+		['bbs.ucrypto.com', 'http://bbs.ucrypto.com:8117'],
+		['bbscoin.cryptohispano.net', 'http://bbscoin.cryptohispano.net/api'],
+		['bbs.codpool.com', 'http://pool1.codpool.com:8116'],
+		['bbs.minersunite.net', 'http://bbs.minersunite.net/api'],
+		['bbs.hashmine.top', 'http://bbs.hashmine.top:8117'],
+		['bbs.proxpool.com', 'http://bbs-pool.proxpool.com/api'],
+		['bbs.minersunite.net', 'https://bb.minersunite.net/api']
+*/
